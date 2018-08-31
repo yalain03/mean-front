dc60bddc7ecdd94b945ab1922f615d7497d844f2
Yves <yalain9@gmail.com>
2018-08-31 00:44:29 -0500

    Set connection to db

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+// db cluster password: JXOlyW9khUiuQSKg
+mongoose.connect('mongodb+srv://yves:JXOlyW9khUiuQSKg@cluster0-h63cl.mongodb.net/mean?retryWrites=true')
+    .then(() => {
+        console.log('db connection successful');
+    })
+    .catch(() => {
+        console.log('db connection failed!');
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,23 +29,24 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
-    console.log(post);
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save();
     res.status(201).json({
         message: 'Post added successfully!'
     });
 });
 
-app.use('/api/posts', (req, res, next) => {
-    const posts = [
-        {id: 'afdaf', title: 'First server-side post', content: 'coming from the server'},
-        {id: 'dfafs', title: 'Second server-side post', content: 'this is the second post'},
-        {id: 'hdghf', title: 'Third server-side post', content: 'another post from the server'}
-    ];
-    res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: posts
-    });
+app.get('/api/posts', (req, res, next) => {
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Posts fetched successfully',
+                posts: documents
+            });
+        });    
 });
 
 module.exports = app;
