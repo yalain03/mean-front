09b048c7613a9b61bcd502ea25f2b666d92350dd
Yves <yalain9@gmail.com>
2018-09-02 19:11:30 -0500

    Token generation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -28,12 +29,14 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
+    let fetchedUser;
     User.findOne({ email: req.body.email}).then(user => {
         if (!user) {
-            return res.status(401).json({
+                return res.status(401).json({
                 message: 'Unauthorized!'
             });
         }
+        fetchedUser = user;
         return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
@@ -42,7 +45,13 @@ router.post('/login', (req, res, next) => {
                 message: 'Unauthorized!'
             });
         }
-        
+        const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
+            'secret_this_should_be_longer',
+            { expiresIn: "1h" }
+        );
+        res.status(200).json({
+            token: token
+        });
     })
     .catch(err => {
         res.status(401).json({
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,4 +18,12 @@ createUser(email: string, password: string) {
     });
 }
 
+login(email: string, password: string) {
+  const authData: AuthData = { email: email, password: password };
+  this.http.post('http://localhost:3000/api/user/login', authData)
+    .subscribe(response => {
+      console.log(response);
+    });
+}
+
 }
